9744e22e75174ec9de7869bf3c1e8ce66367f053
Simar308 <simar308@gmail.com>
2026-03-19 22:09:18 +0530

    switch to api serverless 1

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const app = require('../backend/app');
+const connectDatabase = require('../backend/config/db');
+const cloudinary = require('cloudinary').v2;
+
+// connect DB
+connectDatabase();
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: 'dl8uix1z9',
+  api_key: '786211711552324',
+  api_secret: 'U9wVS1ywp2haJbd_CTOAVK3hrHk',
+  secure: true
+});
+
+// export app (serverless)
+module.exports = app;
